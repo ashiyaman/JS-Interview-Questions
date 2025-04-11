a10fc0d03aa0ad87136a4580ece37fe8de1d4e9e
ashiyaman <ashiya1005@gmail.com>
2025-04-11 10:54:06 +0530

    This keyword

--- index.js
+++ index.js
@@ -1,20 +1,81 @@
-const arr = [1, 2, 3, 4, 5, 6]
+/*const arr = [1, 2, 3, 4, 5, 6]
 
 const newArray = arr.map(num => num + 1)
 console.log(newArray)
 
 const arr1 = arr.forEach(num => num + 2)
-console.log(arr1)   	        //undefined
+console.log(arr1)   	        //undefined*/
 
 //var vs let vs const
 
 function add(){
-    const a  = 10;
+    let a  = 10;
     let isNum = true
     if(isNum){
-        const a = 20
+        let a = 20
         console.log(a)
     }
     console.log(a)
 }
 add()
+
+//This method
+
+/*let ask = {
+    teacher: 'Ashiya',
+    who: function() {
+        console.log(this.teacher)
+    }
+}
+console.log(ask)
+ask.who()                   //implicit binding -> refers to object not property
+
+//Explict binding
+
+function greetingName() {
+    let name = 'Shyam'
+    console.log(this.greeting + ", " + name + "!")
+}
+
+let greeting1 = {
+    greeting: 'Hello'
+}
+
+let greeting2 = {
+    greeting: 'Namaste'
+}
+
+greetingName.call(greeting1)
+greetingName.call(greeting2)
+
+//using .bind
+
+let name = {
+    firstName: 'Ashiya',
+    lastName: 'Amanulla'
+}
+
+let printFullName = function(age, phone) {
+    console.log(this.firstName, this.lastName, age, phone)
+}
+
+printFullName.apply(name, [32, '12345'])*/
+
+/*let printDetails = function(){
+    console.log(this.name + " " + this.age)
+}
+
+let person1 = {
+    name: 'Ashiya',
+    age: 32   
+}
+
+let person1Detail = printDetails.call(person1)
+console.log(person1Detail)
+
+let person2 = {
+    name: 'Rafeeq',
+    age: 35
+}
+
+printDetails.call(person2)*/
